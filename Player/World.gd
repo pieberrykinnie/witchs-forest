4d8ba9444e4527e71d603c9312015759dc8e5c0a
ComTDK <104800594+ComTDK@users.noreply.github.com>
2024-01-02 12:20:50 -0600

    Add files via upload
    Changes: Illustrating multiple bats in the main scene

--- Player/World.gd
+++ Player/World.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
 #if we want to have more bats, we have to call each nodes (could use for loop to do so)
-var bat_in_tree = true
+var bat_in_tree = [true, true, true]
+var bats = [$Bat, $Bat2, $Bat3]
 
 func _process(delta):
-	if bat_in_tree:
-		if $Bat.detect_player:
-			$Bat.attack($Player.position)
+	var bats = [$Bat, $Bat2, $Bat3]
+	for i in range(len(bat_in_tree)):
+		if bat_in_tree[i]:
+			if bats[i].detect_player:
+				bats[i].attack($Player.position)
 	
-		if $Bat.die:
-			bat_in_tree = false
-			$Bat.destroy()
-	
+		if bats[i].die:
+			bat_in_tree[i] = false
+			bats[i].destroy()
